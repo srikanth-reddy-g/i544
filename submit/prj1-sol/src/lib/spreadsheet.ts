1c05f41168e5e80a4b5b7983ce32e5e01fb5a111
Srikanth Reddy Gunukula <sgunukula@binghamton.edu>
2023-06-19 10:34:37 -0400

    commited message

--- submit/prj1-sol/src/lib/spreadsheet.ts
+++ submit/prj1-sol/src/lib/spreadsheet.ts
@@ -1,10 +1,7 @@
-import { ok } from 'assert';
 import { default as parse, CellRef, Ast } from './expr-parser.js';
-
 import { Result, okResult, errResult } from 'cs544-js-utils';
-import exp from 'constants';
 
-//factory method
+// factory method
 export default async function makeSpreadsheet(name: string): Promise<Result<Spreadsheet>> {
   return okResult(new Spreadsheet(name));
 }
@@ -13,42 +10,34 @@ type Updates = { [cellId: string]: number };
 
 export class Spreadsheet {
   readonly name: string;
-  private cells: { [cellId: string]: Ast | undefined };
-  //TODO: add other instance variable declarations
+  cells: { [cellId: string]: Ast }; // Store the parsed expression for each cell
+  values: { [cellId: string]: number }; // Store the evaluated value for each cell
+
   constructor(name: string) {
     this.name = name;
     this.cells = {};
-    //TODO: add initializations for other instance variables
+    this.values = {};
   }
 
-  /** Set cell with id cellId to result of evaluating formula
-   *  specified by the string expr.  Update all cells which are
-   *  directly or indirectly dependent on the base cell cellId.
-   *  Return an object mapping the id's of all updated cells to
-   *  their updated values.
-   *
-   *  Errors must be reported by returning an error Result having its
-   *  code options property set to `SYNTAX` for a syntax error and
-   *  `CIRCULAR_REF` for a circular reference and message property set
-   *  to a suitable error message.
-   */
   async eval(cellId: string, expr: string): Promise<Result<Updates>> {
-    //TODO
-    if (expr === "a1 * a1 + a1" && cellId === "a2")
-    {
-      return okResult({ a2: 22*22 + 22 });
-    }
-    else if (expr === "a1 + a2" && cellId === "a3")
-    {
-      return okResult({ a3: 88,  });
-    }
-    else{
     try {
       const parsedExpr = parse(expr, cellId);
       // console.log(JSON.stringify(parsedExpr, null, 2));
       if (parsedExpr.isOk) {
+        this.cells[cellId] = parsedExpr.val; // Store the parsed expression
         const result = this.evaluateExpression(parsedExpr.val,cellId);
-        return okResult({ [cellId]: result });
+        this.values[cellId] = result; // Store the evaluated value
+        // console.log("Values stored in "+ cellId + " is "+this.values[cellId]);
+        const updates: Updates = { [cellId]: result };
+        for (const key in updates) {
+          if (updates.hasOwnProperty(key)) {
+            const value = updates[key];
+            // console.log(`Key: ${key}, Value: ${value}`);
+          }
+        }
+        // Update dependent cells recursively
+        this.updateDependentCells(cellId, updates);
+        return okResult(updates);
       } else {
         return errResult(parsedExpr, 'SYNTAX');
       }
@@ -56,7 +45,6 @@ export class Spreadsheet {
       return errResult(error, 'SYNTAX');
     }
   }
-}
 
   private evaluateExpression(expr: Ast, baseCellId: string): number {
     if (expr.kind === 'num') {
@@ -72,23 +60,44 @@ export class Spreadsheet {
     } else if (expr.kind === 'ref') {
       const baseCellRef = CellRef.parseRef(baseCellId);
       const cellId = expr.toText(baseCellRef);
-      if (this.cells[cellId] !== undefined) {
-        return this.evaluateExpression(this.cells[cellId]!, cellId);
+      if (this.values[cellId] !== undefined) {
+        return this.values[cellId];
       } else {
-        return 0; // undefined cell is treated as 0
+        return 0; // Return 0 for undefined cells
       }
     } else {
       throw new Error(`Invalid expression: ${expr}`);
     }
   }
-  
-  
-  // return okResult({}); //initial dummy result
 
-  //TODO: add additional methods
+  private updateDependentCells(cellId: string, updates: Updates) {
+    for (const id in this.cells) {
+      if (this.cells.hasOwnProperty(id)) {
+        const expr = this.cells[id];
+        if (this.isDependent(expr, cellId)) {
+          const result = this.evaluateExpression(expr,cellId);
+          this.values[id] = result; // Update the value
+          updates[id] = result; // Add to the updates object
+          // Update dependent cells recursively
+          this.updateDependentCells(id, updates);
+        }
+      }
+    }
+  }
+
+  private isDependent(expr: Ast, cellId: string): boolean {
+    if (expr.kind === 'app') {
+      return expr.kids.some((kid) => this.isDependent(kid, cellId));
+    } else if (expr.kind === 'ref') {
+      const baseCellRef = CellRef.parseRef(cellId);
+      const refCellId = expr.toText(baseCellRef);
+      return refCellId === cellId;
+    } else {
+      return false;
+    }
+  }
+  
 }
-
-//TODO: add additional classes and/or functions
 
 const FNS = {
   '+': (a: number, b: number): number => a + b,
